bcc9c15bb112796e756d6b0a7b63ce1ac6c92c8f
Diego <diego.garcia@ceiba.com.co>
2019-07-06 16:03:51 -0500

    Show usernames

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -3,87 +3,82 @@ import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrls: ['./sidebar.component.scss']
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-    isActive: boolean;
-    collapsed: boolean;
-    showMenu: string;
-    showSubMenu: string;
-    pushRightClass: string;
+  isActive: boolean;
+  collapsed: boolean;
+  showMenu: string;
+  showSubMenu: string;
+  pushRightClass: string;
+  public username = localStorage.getItem('username');
 
-    @Output() collapsedEvent = new EventEmitter<boolean>();
+  @Output() collapsedEvent = new EventEmitter<boolean>();
 
-    constructor(private translate: TranslateService, public router: Router) {
-        this.router.events.subscribe(val => {
-            if (
-                val instanceof NavigationEnd &&
-                window.innerWidth <= 992 &&
-                this.isToggled()
-            ) {
-                this.toggleSidebar();
-            }
-        });
+  constructor(private translate: TranslateService, public router: Router) {
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd && window.innerWidth <= 992 && this.isToggled()) {
+        this.toggleSidebar();
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.isActive = false;
+    this.collapsed = false;
+    this.showMenu = '';
+    this.pushRightClass = 'push-right';
+  }
+
+  eventCalled() {
+    this.isActive = !this.isActive;
+  }
+
+  addExpandClass(element: any) {
+    if (element === this.showMenu) {
+      this.showMenu = '0';
+    } else {
+      this.showMenu = element;
     }
+  }
 
-    ngOnInit() {
-        this.isActive = false;
-        this.collapsed = false;
-        this.showMenu = '';
-        this.pushRightClass = 'push-right';
+  addExpandSubClass(element: any) {
+    if (element === this.showSubMenu) {
+      this.showSubMenu = '1';
+    } else {
+      this.showSubMenu = element;
     }
+    console.log('Hola', this.showSubMenu);
+  }
 
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+    this.collapsedEvent.emit(this.collapsed);
+  }
 
-    eventCalled() {
-        this.isActive = !this.isActive;
-    }
+  isToggled(): boolean {
+    const dom: Element = document.querySelector('body');
+    return dom.classList.contains(this.pushRightClass);
+  }
 
-    addExpandClass(element: any) {
-        if (element === this.showMenu) {
-            this.showMenu = '0';
-        } else {
-            this.showMenu = element;
-        }
-    }
+  toggleSidebar() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle(this.pushRightClass);
+  }
 
-    addExpandSubClass(element: any) {
-        if (element === this.showSubMenu) {
-            this.showSubMenu = '1';
-        } else {
-            this.showSubMenu = element;
-        }
-        console.log('Hola', this.showSubMenu);
+  rltAndLtr() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle('rtl');
+  }
 
-    }
+  changeLang(language: string) {
+    this.translate.use(language);
+  }
 
-    toggleCollapsed() {
-        this.collapsed = !this.collapsed;
-        this.collapsedEvent.emit(this.collapsed);
-    }
-
-    isToggled(): boolean {
-        const dom: Element = document.querySelector('body');
-        return dom.classList.contains(this.pushRightClass);
-    }
-
-    toggleSidebar() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle(this.pushRightClass);
-    }
-
-    rltAndLtr() {
-        const dom: any = document.querySelector('body');
-        dom.classList.toggle('rtl');
-    }
-
-    changeLang(language: string) {
-        this.translate.use(language);
-    }
-
-    onLoggedout() {
-        localStorage.removeItem('isLoggedin');
-        localStorage.removeItem('qwerty');
-    }
+  onLoggedout() {
+    localStorage.removeItem('isLoggedin');
+    localStorage.removeItem('qwerty');
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,64 +7,63 @@ import { AuthGuard } from '../shared';
 import { DataSharingService } from '../services/data-sharing.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    animations: [routerTransition()]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(
-        private router: Router,
-        private connection: ConexionBDService,
-        private tokenService: DataSharingService,
-        private auth: AuthGuard
-    ) { }
+  constructor(
+    private router: Router,
+    private connection: ConexionBDService,
+    private tokenService: DataSharingService,
+    private auth: AuthGuard
+  ) {}
 
-    public user: { correo: string, pass: string } = {
-        correo: '',
-        pass: ''
-    };
+  public user: { correo: string; pass: string } = {
+    correo: '',
+    pass: ''
+  };
 
-    ngOnInit() { }
+  ngOnInit() {}
 
-    onLoggedin() {
-        this.activeSession();
-        this.connection.login(this.user.correo, this.user.pass).subscribe(
-            data => {
-                console.log(data);
+  onLoggedin() {
+    this.connection.login(this.user.correo, this.user.pass).subscribe(
+      data => {
+        console.log(data);
 
-                this.savedUser(data);
-            },
-            err => {
-                console.log(err);
-                Swal.fire('Oppps!', 'Se perdió la conexión con la base de datos. Vuelva a intentarlo.', 'error');
-            }
-        );
+        this.savedUser(data);
+      },
+      err => {
+        console.log(err);
+        Swal.fire('Oppps!', 'Se perdió la conexión con la base de datos. Vuelva a intentarlo.', 'error');
+      }
+    );
+  }
 
-    }
+  savedUser(info: any) {
+    if (info.auth) {
+      this.tokenService.token = info.accessToken;
+      this.connection.getUser().subscribe(data => {
+        if (data.type === 'candidato') {
+          console.log(data);
 
-    savedUser(info: any) {
-        if (info.auth) {
-            this.tokenService.token = info.accessToken;
-            this.connection.getUser().subscribe(
-                data => {
-                    if (data.type === 'candidato') {
-                        this.tokenService.serviceData = data;
-                        this.activeSession();
-                    } else {
-                        Swal.fire('Error de ingreso', 'Usted no es un candidato.', 'error');
-                    }
-                }
-            );
+          this.tokenService.serviceData = data;
+          this.activeSession(data['name'] + ' ' + data['lastname']);
         } else {
-            Swal.fire('Credenciales incorrectas', 'Verifique el correo o la contraseña.', 'error');
+          Swal.fire('Error de ingreso', 'Usted no es un candidato.', 'error');
         }
+      });
+    } else {
+      Swal.fire('Credenciales incorrectas', 'Verifique el correo o la contraseña.', 'error');
     }
+  }
 
-    activeSession() {
-        localStorage.setItem('isLoggedin', 'true');
-        localStorage.setItem('qwerty', this.tokenService.token);
-        this.auth.canActivate();
-        this.router.navigate(['/dashboard']);
-    }
+  activeSession(username: string) {
+    localStorage.setItem('username', username);
+    localStorage.setItem('isLoggedin', 'true');
+    localStorage.setItem('qwerty', this.tokenService.token);
+    this.auth.canActivate();
+    this.router.navigate(['/dashboard']);
+  }
 }
